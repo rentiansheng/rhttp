acf6ab5b7673f82d523e2e40acf85d8d59a92322
rentiansheng <rentiansheng@163.com>
2014-01-15 14:06:31 -0800

    code

---
+++ http/http_send_html.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) Reage
+ * blog:http://www.rhttp.cn
+ */
+#ifndef _HTTP_SEND_HTML_H_INCLUDE
+#define _HTTP_SEND_HTML_H_INCLUDE
+
+#include "base.h"
+#include "http_request.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+
+int produce_http_header(http_connect_t *con);
+
+int http_send_header(http_connect_t *con);
+
+int http_send_body(http_connect_t *con);
+
+
+
+
+#endif

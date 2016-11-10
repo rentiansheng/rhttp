3fa80bf8d6df4a4245bc36cea86009da309429ea
Reage <rentiansheng@163.com>
2016-11-10 20:17:36 +0800

    add str

---
+++ core/str.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) Reage
+ * blog:http://www.ireage.com
+ */
+
+#ifndef _CORE_STR_H_
+#define _CORE_STR_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pool.h"
+
+typedef  struct string {
+    char *ptr;
+    int len;
+}string;
+
+string * string_init(pool_t *p);
+string * string_init_from_str(pool_t *p, char *b, size_t len);
+string * string_init_from_ptr(pool_t *p, char *ptr, size_t len);
+int string_compare(const string *s1, const string *s2);
+void string_copy_str_n(const string *b, char *s1, int n);
+int  string_get_line(char  *start, char *end, string *dst);
+void string_get_word_with_split(string * src, string *dst, char split);
+
+
+#endif
